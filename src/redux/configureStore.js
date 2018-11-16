16f12a7870fe40e021e4608b48aec73e7afe6938
Jimmy Jun <jimmyjun@Jimmys-MacBook-Pro-2.local>
2018-11-15 23:09:12 -0800

    Finished prelim set up tasks

---
+++ src/redux/configureStore.js
@@ -0,0 +1,4 @@
+import { createBrowserHistory } from 'history';
+import { applyMiddleware, compose, createStore } from 'redux';
+import { routerMiddleware } from 'connected-react-router';
+import rootReducer from './reducers/rootReducer';
